4aa8ef4a1e0a0ce12f1ba36f52134f4d1393fe1f
Paul Tiede <ptiede91@gmail.com>
2023-05-30 12:19:24 -0400

    Add Rice distribution for amplitudes

--- src/amplitude.jl
+++ src/amplitude.jl
@@ -1,4 +1,4 @@
-export AmplitudeLikelihood
+export AmplitudeLikelihood, RiceAmplitudeLikelihood
 
 struct AmplitudeLikelihood{V1,V2<:Union{AbstractVector, AbstractPDMat},W} <: AbstractVLBIDistributions
     μ::V1
@@ -76,5 +76,58 @@ function Distributions._rand!(rng::Random.AbstractRNG, d::AmplitudeLikelihood{<:
     randn!(rng, x)
     PDMats.unwhiten!(d.Σ, x)
     x .+= d.μ
+    return x
+end
+
+struct RiceAmplitudeLikelihood{V1,V2<:AbstractVector,W} <: AbstractVLBIDistributions
+    μ::V1
+    Σ::V2
+    lognorm::W
+end
+
+Base.length(d::RiceAmplitudeLikelihood) = length(d.μ)
+Base.eltype(d::RiceAmplitudeLikelihood) = promote_type(eltype(d.μ), eltype(d.Σ))
+Dists.insupport(::RiceAmplitudeLikelihood, x) = true
+
+"""
+    RiceAmplitudeLikelihood(μ, Σ::Union{AbstractVector, Diagonal})
+
+Forms the likelihood for amplitudes from the mean vector `μ` and
+the diagonal covariance matrix `Σ`. `Σ` can either be a Diagonal matrix or
+a vector whose entries are the variance for each data point.
+
+# Notes
+This is the correct likelihood distribution for visibility amplitudes, but it is slower
+than the Gaussian approximation `AmplitudeLikelihood`. Additionally, i
+"""
+function RiceAmplitudeLikelihood(μ::AbstractVector, Σ::AbstractVector)
+    lognorm = -2*sum(log, Σ)
+    return RiceAmplitudeLikelihood(μ, Σ, lognorm)
+end
+
+function RiceAmplitudeLikelihood(μ::AbstractVector, Σ::Diagonal)
+    return RiceAmplitudeLikelihood(μ, diag(Σ))
+end
+
+_L12(x) = exp(x/2)*((1-x)*besseli0(-x/2) - x*besseli1(-x/2))
+
+function unnormed_logpdf(d::RiceAmplitudeLikelihood, x::AbstractVector)
+    return sum(zip(d.μ, d.Σ, x)) do (μ, Σ, xx)
+        # we use besseli0x for numerical stability for high SNR points
+        return -(xx^2 + μ^2)*inv(2*Σ) + log(xx*besseli0x(xx*μ/Σ)) - xx*μ/Σ
+    end
+end
+
+Dists.mean(d::RiceAmplitudeLikelihood) = sqrt.(d.Σ .* π/2).*_L12.(-d.μ.^2 .* inv.(2 .* d.Σ))
+function Dists.var(d::RiceAmplitudeLikelihood)
+    T = eltype(d)
+    return 2*d.Σ .+ d.μ.^2 - (T(π).*d.Σ/2).*_L12.(-d.μ.^2 .* inv.(2*d.Σ)).^2
+end
+
+
+function Distributions._rand!(rng::Random.AbstractRNG, d::RiceAmplitudeLikelihood{<:AbstractVector, <:AbstractVector}, x::AbstractVector)
+    randn!(rng, x)
+    # sample from a normal gaussian and abs.
+    x .= abs.(complex.(x.*sqrt.(d.Σ) .+ d.μ, randn.(rng).*sqrt.(d.Σ)))
     return x
 end
