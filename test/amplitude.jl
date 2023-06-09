a69b2fed164744c50541403b7992736aa0739edd
Paul Tiede <ptiede91@gmail.com>
2023-06-09 11:05:51 -0400

    Add amplitude tests

--- test/amplitude.jl
+++ test/amplitude.jl
@@ -37,7 +37,6 @@ end
 
 @testset "Rice Amplitude Likelihood" begin
 
-    m = central_fdm(5,1)
 
     μ = rand(50)
     Σ = 5 .* Diagonal(ones(50))
@@ -52,19 +51,14 @@ end
     @test logpdf(dv, x) ≈ logpdf(dv, x)
     @test logpdf(dv2, x) ≈ logpdf(dv2, x)
 
-    test_rrule(AmplitudeLikelihood, μ, Σd)
+    test_rrule(VLBILikelihoods.unnormed_logpdf, dv, x)
 
-    @inferred Zygote.gradient(logdensityof(dv), x)
-    @inferred Zygote.gradient(logdensityof(dv2), x)
-
-    f(x, μ, Σ) = logdensityof(AmplitudeLikelihood(μ, Σ), x)
-
-
-    gvz  = Zygote.gradient(f, x, μ, Σd)
-    gvz2 = Zygote.gradient(f, x, μ, Diagonal(Σ))
-    @inferred Zygote.gradient(f, x, μ, Σ)
-
-    gfdz  = grad(m, f, x, μ, Σd)
-    @test all(isapprox.(gvz, gfdz))
-
+    @testset "High SNR" begin
+        μ = 100*rand(50) .+ 2.0
+        Σ = 1e-6*rand(50)
+        d = RiceAmplitudeLikelihood(μ, Σ)
+        dN = MvNormal(μ, Diagonal(Σ))
+        x = rand(d)
+        @test isapprox(logpdf(d, x), logpdf(dN, x), atol=1e-3)
+    end
 end
